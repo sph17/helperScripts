2cf19b3d0c3ff1b5de2be0df48b2eb3f14b3143e
Stephanie Hao <sphao@bu.edu>
2026-01-09 17:30:59 -0500

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -84,4 +84,18 @@ pretty_kable <- function(
   kb
 }
 
+pretty_kable_summary <- function(x, scroll = TRUE, height = "300px", align = NULL) {
+  
+  # derive caption from column names
+  caption <- paste(colnames(x), collapse = ", ")
+  
+  pretty_kable(
+    x,
+    caption = caption,
+    scroll = scroll,
+    height = height,
+    align = align
+  )
+}
+
 
